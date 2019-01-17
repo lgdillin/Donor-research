6e869f9a474bf9e80eee001d06c64eb723b37cdd
lgdillin <mailgd9@gmail.com>
2019-01-17 09:25:16 -0600

    some work

--- main.py
+++ main.py
@@ -5,17 +5,43 @@ import pandas as pd
 
 from pandas import pivot_table
 
-donors = pd.read_csv('donors2016.csv')
+# for donors2020.csv
+donors_2020 = pd.read_csv('donors2020.csv')
+donors_2020 = donors_2020.groupby([
+        'contributor_name', 
+        'contributor_state', 
+        'contributor_city', 
+        'contributor_zip',
+        'contributor_street_1', 
+        'contributor_employer',
+        'contributor_occupation'
+    ], as_index = False)['contributor_aggregate_ytd'].max()
+donors_2020 = pd.DataFrame(donors_2020)
+print(donors_2020.head())
 
-#collect_donations = donors.groupby(['contbr_nm', 'contbr_city', 'contbr_st', 'contbr_employer', 'contb_receipt_amt'], as_index = False)['contb_receipt_amt'].sum()
+print('')
+print('-------------------------------')
+print('')
 
-cd = pivot_table(donors, values = ['contb_receipt_amt', 'total_donations'], index = ['contbr_nm', 'contbr_city', 'contbr_st', 'contbr_employer'],
-    aggfunc = {'total_donations': np.sum})
+# for donors2016.csv
+donors_2016 = pd.read_csv('donors2016.csv')
+donors_2016 = donors_2016.groupby([
+        'contbr_nm',
+        'contbr_st',
+        'contbr_city',
+        'contbr_zip',
+        'contbr_employer',
+        'contbr_occupation'
+    ], as_index = False)['contb_receipt_amt'].sum()
+donors_2016 = pd.DataFrame(donors_2016)
+print(donors_2016.head())
+
+# cd = pivot_table(donors, values = ['contb_receipt_amt', 'total_donations'], index = ['contbr_nm', 'contbr_city', 'contbr_st', 'contbr_employer'], aggfunc = {'total_donations': np.sum})
 
 # collect_donations = donors.groupby(['contbr_st'], as_index = False)['contb_receipt_amt'].sum()
 #collect_donations = pd.DataFrame(collect_donations)
 #collect_donations.to_csv('pivot.csv', columns = ['contbr_nm', 'contbr_city', 'contbr_st', 'contbr_employer', 'contb_receipt_amt'], index = False)
-print(cd.head())
+#print(collect_donations.head())
 # collect_donations.to_csv('pivot.csv',)
 
 # pie_chart = pygal.Pie()
